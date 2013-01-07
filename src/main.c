3e1732f48c9599fc1d7bbbdd5cc6849f883c4f93
Zane Ashby <zane.a@demonastery.org>
2013-01-07 13:36:33 +1300

    Fixed LICENSE.txt and added license header.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,23 @@
 //
-// NPAPI implementation of Wacom Web Plugin.
+// WacomWebPlugin - An implementation of the Wacom Web API on Linux.
+// Copyright (C) 2013  Zane Ashby <zane.a@demonastery.org>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+//
+// Entrypoint for plugin.
 //
 
 #include "common.h"
